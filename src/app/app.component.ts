d33a9642d6ef66ddc3c932224c5dbdede1c15e2e
zar <zmfy3@mail.umsl.edu>
2019-04-14 17:46:21 -0500

    added disabled attribute to button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,15 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'parksAndRec';
+  zipcode;
 
   submit() {
     console.log('submit button clicked');
+    console.log(this.zipcode);
+
+
+
+
+
+
   }
 
 
 
-
 }
-
-
